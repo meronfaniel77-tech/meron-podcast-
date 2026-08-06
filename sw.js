d4f25d11f92f31c5b51861dbd40abd21f4bbb3f3
meronfaniel77-tech <meronfaniel77@gmail.com>
2026-08-06 11:05:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
 
+const CACHE_NAME = 'meron-v1';
+
+// Evento di installazione
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/logo.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Evento di attivazione
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+// Evento Fetch
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
